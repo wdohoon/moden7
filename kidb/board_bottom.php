b26f444457624e2914bcffcf993fcdeeabfc3146
{wdohoon} <{dnjsehgns98@gmail.com}>
2024-04-30 15:56:34 +0900

    teamhope

--- kidb/board_bottom.php
+++
@@ -1,4 +0,0 @@
-</div>
-
-<?php
-include_once(G5_PATH.'/tail.php');
--- kidb/extend/user.config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	if ($member['mb_id'] == 'manager1') $is_admin = 'super';
-	if ($member['mb_id'] == 'manager2') $is_admin = 'super';
-	if ($member['mb_id'] == 'manager3') $is_admin = 'super';
-	if ($member['mb_id'] == 'manager4') $is_admin = 'super';
-?>
